cc0d4d7e835a5d8b22cb72396d6e109aef9e9506
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-01-13 23:41:41 -0700

    added vanilla nodejs files

---
+++ vanilla_node_js/routes.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>')
+        res.write('</html>');
+        res.end();
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    // console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body>Hello!!!!</body>')
+    res.write('</html>');
+    res.end();
+};
+
+
+// Exports
+module.exports = {
+    handler: requestHandler,
+    someText: 'Some hard coded text'
+};
+
+// OR
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
+
