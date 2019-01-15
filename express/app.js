caf449eae7c2b23baa995d9027f5627d29a51cb7
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-01-15 00:11:02 -0700

    finished pug templating

--- express/app.js
+++ express/app.js
@@ -4,9 +4,10 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
+app.set('view engine', 'pug');
+app.set('views', 'views')
 
-
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
 
@@ -19,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 
